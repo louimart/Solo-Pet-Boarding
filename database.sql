89ec5877a0b110c14011c492a9d24bf58a66b627
Louis Martin <louimart@gmail.com>
2024-03-19 09:26:09 -0500

    database queries updated for revisions to table structure. renaming of columns for consistency. vaccine broken out onto its own table.

--- database.sql
+++ database.sql
@@ -6,28 +6,41 @@ CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
     "first_name" VARCHAR (20) NOT NULL,
     "last_name" VARCHAR (20) NOT NULL,
     "email" VARCHAR (50) NOT NULL,
-    "phone_number" VARCHAR (20) NOT NULL,
+    "phone" VARCHAR (20) NOT NULL,
     "street" VARCHAR (50) NOT NULL,
     "city" VARCHAR (25) NOT NULL,
-    "state" VARCHAR (25) NOT NULL
+    "state" VARCHAR (25) NOT NULL,
+    "user_type" VARCHAR (25)
 );
-
-DROP TABLE "user";
 
 CREATE TABLE "pet" (
     "id" SERIAL PRIMARY KEY,
     "dog" BOOLEAN DEFAULT FALSE,
     "cat" BOOLEAN DEFAULT FALSE,
     "pet_name" VARCHAR (25) NOT NULL,
+    "breed" VARCHAR (25) NOT NULL,
     "age" INT NOT NULL,
     "weight" INT NOT NULL,
     "detail" VARCHAR (500) NOT NULL,
-    "rabies" BOOLEAN DEFAULT FALSE,
-    "bordetella" BOOLEAN DEFAULT FALSE,
-    "distemper" BOOLEAN DEFAULT FALSE
+    "vaccine a" BOOLEAN DEFAULT FALSE,
+    "vaccine b" BOOLEAN DEFAULT FALSE,
+    "vaccine c" BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE "vaccine" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (25) NOT NULL,
+	"is_dog" BOOLEAN,
+	"is_cat" BOOLEAN
 );
 
 CREATE TABLE "user_pets" (
@@ -36,47 +49,48 @@ CREATE TABLE "user_pets" (
     "pet_id" INT REFERENCES "pet"
 );
 
-CREATE TABLE "request_meet" (
-    "id" SERIAL PRIMARY KEY,
-    "dog" BOOLEAN DEFAULT FALSE.
-    "cat" BOOLEAN DEFAULT FALSE,
-    "first_name" VARCHAR (20) NOT NULL,
-    "last_name" VARCHAR (20) NOT NULL,
-    "pet_name" VARCHAR (20) NOT NULL,
-    "email" VARCHAR (50) NOT NULL,
-    "phone_number" VARCHAR (20) NOT NULL,
-    "details" VARCHAR (500) NOT NULL,
-    "confirmed" BOOLEAN DEFAULT FALSE
-);
-
-CREATE TABLE "payments" (
-    "id" SERIAL PRIMARY KEY,
-    "boarding_id" INT REFERENCES "request_boarding",
-    "user_id" INT REFERENCES "request_boarding",
-    "fee" INT REFERENCES "request_boarding",
-    "outstanding" BOOLEAN DEFAULT TRUE
+CREATE TABLE "pet_boarding" (
+	"id" SERIAL PRIMARY KEY,
+	"request_id" INT REFERENCES "request_boarding",
+	"user_id" INT REFERENCES "user",
+	"pet_id" INT NOT NULL
 );
 
 CREATE TABLE "request_boarding" (
-    "id" SERIAL PRIMARY KEY,
-    "user_id" INT REFERENCES _____,
-    "pet_1" VARCHAR (25) REFERENCES "pet",
-    "pet_2" VARCHAR (25) REFERENCES "pet",
-    "start_date" DATE NOT NULL,
-    "end_date" DATE NOT NULL,
-    "pickup" BOOLEAN DEFAULT FALSE,
-    "dropoff" BOOLEAN DEFAULT FALSE,
-    "details" VARCHAR (500) NOT NULL,
-    "fee" INT NOT NULL
-);
+	"id" SERIAL PRIMARY KEY,
+	"start_date" DATE NOT NULL,
+	"end_date" DATE NOT NULL,
+	"pickup" BOOLEAN DEFAULT FALSE,
+	"dropoff" BOOLEAN DEFAULT FALSE,
+	"details" VARCHAR (500) NOT NULL,
+	"fee" INT NOT NULL
+	);
 
 CREATE TABLE "reviews" (
-    "id" SERIAL PRIMARY KEY,
-    "user_id" INT REFERENCES "request_boarding",
-    "pet_id" INT REFERENCES "pet",
-    "boarding_id" INT REFERENCES "request_booking",
-    "stars" INT NOT NULL,
-    "comments" VARCHAR (500) NOT NULL
+	"id" SERIAL PRIMARY KEY,
+	"boarding_id" INT NOT NULL,
+	"star_rating" INT,
+	"comments" VARCHAR (500) NOT NULL
+);
+
+CREATE TABLE "request_meet" (
+	"id" SERIAL PRIMARY KEY,
+	"dog" BOOLEAN DEFAULT FALSE NOT NULL,
+	"cat" BOOLEAN DEFAULT FALSE NOT NULL,
+	"first_name" VARCHAR (20) NOT NULL,
+	"last_name" VARCHAR (20) NOT NULL,
+	"email" VARCHAR (50) NOT NULL,
+	"phone" VARCHAR (20) NOT NULL,
+	"details" VARCHAR (500) NOT NULL,
+	"confirmation" BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE "payments" (
+	"id" SERIAL PRIMARY KEY,
+	"boarding_id" INT NOT NULL,
+	"client_id" INT NOT NULL,
+	"fee" INT NOT NULL,
+	"outstanding" BOOLEAN DEFAULT TRUE
 );
 
 INSERT INTO "user" ("username", "password", "first_name", "last_name", "email", "phone_number", "street", "city", "state")
@@ -85,6 +99,24 @@ VALUES
 ('catlover', 'iheartcat123', 'john', 'smith', 'jsmith@gmail.com', '6121234567', '1234 maple lane', 'minneapolis', 'mn'),
 ('loverofanimals', 'iheartAnimals123', 'joe', 'jones', 'jjones@gmail.com', '7631238901', '4567 jolly lane', 'minneapolis', 'mn')
 ;
+
+INSERT INTO "pet" ("dog", "cat", "pet_name", "age", "weight", "detail", "rabies", "bordetella", "distemper")
+VALUES
+(TRUE, FALSE, 'bailey', 7, 15, 'needs to go on walk to poop and likes to sleep in his own bed', TRUE, TRUE, TRUE),
+(FALSE, TRUE, 'sina', 10, 15, 'half can of wet food twice a day. shot once a day', TRUE, TRUE, TRUE)
+;
+
+DROP TABLE "user";
+DROP TABLE "pet";
+DROP TABLE "user_pets";
+DROP TABLE "vaccine";
+DROP TABLE "pet_boarding";
+DROP TABLE "request_boarding";
+DROP TABLE "request_meet";
+DROP TABLE "reviews";
+DROP TABLE "payments";
+
+DROP TABLE "user", "pet", "user_pets", "vaccine", "pet_boarding", "request_boarding", "request_meet", "reviews", "payments";
 
 INSERT INTO "pet" ("dog", "cat", "pet_name", "age", "weight", "detail", "rabies", "bordetella", "distemper")
 VALUES

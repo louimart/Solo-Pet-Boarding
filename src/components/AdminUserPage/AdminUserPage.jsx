6fdd0bb10ab23ec489f360d58ac9dd1340141d75
Louis Martin <louimart@gmail.com>
2024-03-24 11:53:39 -0500

    userpage conditional rendering for Admin vs Client functionaing.

---
+++ src/components/AdminUserPage/AdminUserPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import LogOutButton from '../LogOutButton/LogOutButton';
+import { useSelector } from 'react-redux';
+
+function AdminUserPage() {
+  // this component doesn't do much to start, just renders some user reducer info to the DOM
+  const user = useSelector((store) => store.user);
+
+  <div className="container">
+    <h2>Hello, {user.username}!</h2>
+    <p>Your ID is: {user.id}</p>
+    <p>User Type: {user.user_type}</p>
+    <LogOutButton className="btn" />
+  </div>;
+}
+
+// this allows us to use <App /> in index.js
+export default AdminUserPage;
